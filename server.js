3b1114cb83d66e284a450e18b8fa893033844f0b
Robat Williams <rwilliams@scottlogic.com>
2019-08-05 09:46:11 +0100

    Log headers

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 
 app.use((req, res, next) => {
+  console.log(req.url);
+  console.log(req.headers);
+
   if (req.url.endsWith('.txt')) {
     setTimeout(next, 5000);
   } else {
